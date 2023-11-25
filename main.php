93a22350d2b8b784536a6737388c5f0c367cb8b6
caroris <72808005+caroris@users.noreply.github.com>
2023-11-25 13:37:36 +0100

    display temperature closest to zero
    if equal display the positive temperature

--- main.php
+++ main.php
@@ -5,15 +5,31 @@
  **/
 
 // $n: the number of temperatures to analyse
-fscanf(STDIN, "%d", $n);
+fscanf(STDIN, "%d", $nbrTemp);
 $inputs = explode(" ", fgets(STDIN));
-for ($i = 0; $i < $n; $i++)
+
+$tempClosestToZero = $inputs[0];
+
+if ($nbrTemp === 0) {
+	$tempClosestToZero = 0;
+}
+
+for ($i = 0; $i < $nbrTemp; $i++)
 {
-    $t = intval($inputs[$i]); // a temperature expressed as an integer ranging from -273 to 5526
+    $tempValue = intval($inputs[$i]); // a temperature expressed as an integer ranging from -273 to 5526
+	
+	if (abs($tempValue) < abs($tempClosestToZero)) {
+		$tempClosestToZero = $tempValue;
+	}
+	
+	if (abs($tempValue) === abs($tempClosestToZero)) {
+		$tempClosestToZero = $tempValue > $tempClosestToZero ? $tempValue : $tempClosestToZero;
+	}
+	
 }
 
 // Write an answer using echo(). DON'T FORGET THE TRAILING \n
 // To debug: error_log(var_export($var, true)); (equivalent to var_dump)
 
-echo("result\n");
+echo("$tempClosestToZero\n");
 ?>
