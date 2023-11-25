263def640c108f066ca30f6827fba8c10a74207b
caroris <72808005+caroris@users.noreply.github.com>
2023-11-25 13:05:15 +0100

    first commit

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+// $n: the number of temperatures to analyse
+fscanf(STDIN, "%d", $n);
+$inputs = explode(" ", fgets(STDIN));
+for ($i = 0; $i < $n; $i++)
+{
+    $t = intval($inputs[$i]); // a temperature expressed as an integer ranging from -273 to 5526
+}
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("result\n");
+?>
